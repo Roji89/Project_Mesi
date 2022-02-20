739bf880dd94f9dc6b72d8f5784ef44413dc646b
Anny <anny_barrero@hotmail.com>
2022-02-20 13:06:09 +0100

    add commun folder with footer and header components

--- MESI-Frontend/src/app/app.module.ts
+++ MESI-Frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './commun/header/header.component';
+import { FooterComponent } from './commun/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
