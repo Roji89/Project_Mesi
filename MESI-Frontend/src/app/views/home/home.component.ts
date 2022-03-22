8e13773644fe7690bcd2b634b9e53a87095c915e
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-03-22 22:25:10 +0100

    Remove unused injection

--- MESI-Frontend/src/app/views/home/home.component.ts
+++ MESI-Frontend/src/app/views/home/home.component.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+
+  constructor() {}
 
   ngOnInit(): void {}
 }
