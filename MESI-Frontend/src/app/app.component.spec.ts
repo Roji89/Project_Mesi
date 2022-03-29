a7f481efc788f1b9287c5b85fe6af768f2fbb839
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-03-29 11:23:28 +0200

    Mesi 10 GitHub actions (#12)
    * Add run tests action
    
    * Update task
    
    * fix missing typo
    
    * Remove dash symbol
    
    * Add working-directory
    
    * Add node version 14
    
    * Add test command
    
    * Update run script
    
    * Update name
    
    * Fix wrong naming
    
    * Remove on push
    
    * Add components declarations

--- MESI-Frontend/src/app/app.component.spec.ts
+++ MESI-Frontend/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './commun/footer/footer.component';
+import { NavBarComponent } from './commun/nav-bar/nav-bar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +11,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavBarComponent,
+        FooterComponent
       ],
     }).compileComponents();
   });
