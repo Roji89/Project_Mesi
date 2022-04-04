57baccdfb0e20560d006fbdf4b9f6151b8fbdfe7
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-04 22:24:09 +0200

    Add communication with backend for login

--- MESI-Frontend/src/app/views/login/login.component.ts
+++ MESI-Frontend/src/app/views/login/login.component.ts
@@ -1,28 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
+  error: string = ''
 
-  constructor(private authService: AuthService, private router: Router) { }
-  ngOnInit(): void {
+  loginForm = this.formBuilder.group({
+    email: '',
+    password: ''
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+  
+  ngOnInit(): void {}
+
+  showAlert(message: string): void {
+    console.log(message)
+    this.error = message
+  }
+
+  hideAlert(): void {
+    this.error = '';
   }
 
   /** method called when user try to login */
-  onSubmitLoginForm() {
+  onSubmit() {
     this.authService
-    .login('anny_barrero@hotmail.com', 'unpassword')
-    .then(() => {
-      //When promise is succesful
-      this.router.navigateByUrl('user-board');
-    });
+      .login(this.loginForm.value["email"], this.loginForm.value["password"]).subscribe({
+        next: (v) => this.navigateAfterLogin(v.token),
+        error: (e) => {
+        e.status !== 0
+          ? this.showAlert(e.error)
+          : this.showAlert("No network") // Handle network error
+        }})
   }
 
-
+  navigateAfterLogin(token: string): void {
+    this.authService.token = token;
+    this.router.navigate([''])
+  }
 }
