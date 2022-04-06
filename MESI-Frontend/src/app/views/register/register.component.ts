4a07b9e9354b3ccdf313eae854b72b96a20c8e49
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-06 22:21:36 +0200

    Add onSubmit method

--- MESI-Frontend/src/app/views/register/register.component.ts
+++ MESI-Frontend/src/app/views/register/register.component.ts
@@ -1,27 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   registerForm = this.formBuilder.group({
     firstName: '',
     lastName: '',
     email: '',
-    password: ''
-  })
+    password: '',
+  });
+  error: string = '';
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showAlert(message: string): void {
+    this.error = message
+  }
+
+  hideAlert(): void {
+    this.error = '';
   }
 
   onSubmit(): void {
-    
-  }
+    let user: User = new User(
+      this.registerForm.value['firstName'],
+      this.registerForm.value['lastName'],
+      this.registerForm.value['email'],
+      this.registerForm.value['password']
+    );
 
+    this.authService.register(user).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => this.showAlert(e.error),
+    });
+  }
 }
