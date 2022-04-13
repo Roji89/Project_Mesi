c076cd3b7719e7901ab0dd0b3905971bc42ed88c
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-13 18:08:08 +0200

    Update user model

--- MESI-Frontend/src/app/models/user.model.ts
+++ MESI-Frontend/src/app/models/user.model.ts
@@ -1,8 +1,8 @@
 export interface User {
-    id?: number;
+    _id: string;
     firstName?: string;
     lastName?: string;
-    email: string;
-    password: string;
-    token?: string;
+    email?: string;
+    password?: string;
+    token: string;
 }
