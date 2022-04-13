fbf9a7a298ac866573446bce42ab5b052fb42321
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-13 18:08:01 +0200

    Update routerlink navbar

--- MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
+++ MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  
   constructor() {}
 
   ngOnInit(): void {}
-
 }
