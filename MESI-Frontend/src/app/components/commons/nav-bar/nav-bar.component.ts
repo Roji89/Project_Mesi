ebe73ea488ee354162d4121677ce7a0a92380302
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-04-19 21:54:40 +0200

    Implement logout into navbar

--- MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
+++ MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataSharingService } from 'src/app/services/data-sharing/data-sharing.service';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  isUserLoggedIn?: boolean;
+
+  constructor(
+    private authService: AuthService,
+    private dataSharingService: DataSharingService,
+    private router: Router
+  ) {
+    this.dataSharingService.userIsLoggedIn.subscribe(value => this.isUserLoggedIn = value)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClickLogout(): void{
+    this.authService.clearUserCredentials();
+    this.dataSharingService.userIsLoggedIn.next(false);
+  }
 }
