d00fcafdd02a9e6b2dd50544af96a09538122c82
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-05-08 19:15:58 +0200

    Refactor with BASE_URL & _id

--- MESI-Frontend/src/app/components/home/products/products.component.ts
+++ MESI-Frontend/src/app/components/home/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/products/product.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-products',
@@ -8,6 +9,8 @@ import { ProductService } from 'src/app/services/products/product.service';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+
+  BASE_URL: string = environment.API_URL;
   products: Product[] = [];
 
   constructor(private productService: ProductService) {}
@@ -15,11 +18,13 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.getProducts();
   }
-
+  
   getProducts() {
     this.productService.productList()
     .subscribe({
-      next: (response: Product[]) => this.products = response,
+      next: (data: any) => {
+        this.products = data.data
+      },
       error: (e) => console.log(e)})
     }
 }
