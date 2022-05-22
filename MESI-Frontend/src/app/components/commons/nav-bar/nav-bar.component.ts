8a4a835fbf5d6589dc221a9dfb32a34f4f1c91af
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-05-22 21:29:49 +0200

    Handle search on navbar

--- MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
+++ MESI-Frontend/src/app/components/commons/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataSharingService } from 'src/app/services/data-sharing/data-sharing.service';
 import { AuthService } from '../../../services/auth/auth.service';
@@ -9,22 +10,33 @@ import { AuthService } from '../../../services/auth/auth.service';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  searchForm = this.formBuilder.group({
+    search: '',
+  });
 
   isUserLoggedIn?: boolean;
 
   constructor(
     private authService: AuthService,
     private dataSharingService: DataSharingService,
+    private formBuilder: FormBuilder,
     private router: Router
   ) {
-    this.dataSharingService.userIsLoggedIn.subscribe(value => this.isUserLoggedIn = value)
+    this.dataSharingService.userIsLoggedIn.subscribe(
+      (value) => (this.isUserLoggedIn = value)
+    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onClickLogout(): void{
+  onClickLogout(): void {
     this.authService.clearUserCredentials();
     this.dataSharingService.userIsLoggedIn.next(false);
+  }
+
+  onSubmit(): void {
+    this.router.navigate(['search'], {
+      queryParams: { q: this.searchForm.value['search'] },
+    });
   }
 }
