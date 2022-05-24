52fb8e0f69371820df9adee24b3b38c94bdaabb8
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-05-24 22:01:01 +0200

    Finish script

--- MESI-Backend/db/script.js
+++ MESI-Backend/db/script.js
@@ -1,20 +1,32 @@
-const { products } = require('./data');
-
-const MongoClient = require('mongodb').MongoClient;
-const MONGO_URI = "mongodb://root:example@localhost:27017"
+require("dotenv").config();
+const { products, users } = require("./data");
+const MongoClient = require("mongodb").MongoClient;
 
 const init = async () => {
-  
-  MongoClient.connect(MONGO_URI, function(err, db) {
+  MongoClient.connect(process.env.MONGO_URI, function (err, db) {
     if (err) throw err;
+
+    // Reset database
     const dbo = db.db("test");
-    dbo.collection('products').deleteMany({});
-    dbo.collection("products").insertMany(products, function(err, res) {
+
+    dbo.collection("products").deleteMany({});
+
+    dbo.collection("products").insertMany(products, function (err, res) {
       if (err) throw err;
+
       console.log(`${products.length} PRODUCTS INSERTED WITH SUCCESS`);
+    });
+
+    dbo.collection("users").deleteMany({});
+
+    dbo.collection("users").insertMany(users, function (err, res) {
+      if (err) throw err;
+
+      console.log(`${users.length} USERS INSERTED WITH SUCCESS`);
       db.close();
     });
-  }); 
+
+  });
 };
 
 init();
